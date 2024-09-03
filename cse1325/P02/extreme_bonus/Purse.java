09bfa999b9d64ee59d80df42396a9358ae00389f
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-09-03 08:58:22 +0000

    Updated Purse class in Extreme Bonus

--- cse1325/P02/extreme_bonus/Purse.java
+++ cse1325/P02/extreme_bonus/Purse.java
@@ -37,3 +37,40 @@ public class Purse {
         }
     }
 }
+
+
+public class Purse {
+    private List<Coin> coins = new ArrayList<>();
+
+    public void addCoin(Coin coin) {
+        coins.add(coin);
+    }
+
+    public double totalValue() {
+        double total = 0;
+        for (Coin coin : coins) {
+            total += coin.getValue();
+            System.out.println(coin); // Print coin details
+        }
+        return total;
+    }
+
+    public double totalWeight() {
+        double total = 0;
+        for (Coin coin : coins) {
+            total += coin.getWeight();
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Purse purse = new Purse();
+        purse.addCoin(new Coin(Denomination.PENNY, 1980));
+        purse.addCoin(new Coin(Denomination.NICKEL, 1990));
+        purse.addCoin(new Coin(Denomination.DIME, 2000));
+        purse.addCoin(new Coin(Denomination.QUARTER, 2010));
+        
+        System.out.println("Total value: " + purse.totalValue());
+        System.out.println("Total weight: " + purse.totalWeight() + " grams");
+    }
+}
