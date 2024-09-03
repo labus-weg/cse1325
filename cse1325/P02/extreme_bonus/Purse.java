bc456330a1ac35aa7ebc57930c913c4fd3ed5e21
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-09-03 08:49:58 +0000

    Updated Purse class to calculate and display total weight

---
+++ cse1325/P02/extreme_bonus/Purse.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Purse {
+    private List<Coin> coins = new ArrayList<>();
+
+    public void addCoin(Coin coin) {
+        coins.add(coin);
+    }
+
+    public double totalValue() {
+        double total = 0.0;
+        for (Coin coin : coins) {
+            total += coin.getValue();
+        }
+        return total;
+    }
+
+    public double totalWeight() {
+        double total = 0.0;
+        for (Coin coin : coins) {
+            total += coin.getWeight();
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Purse purse = new Purse();
+        purse.addCoin(new Coin(Denomination.PENNY, 1982));
+        purse.addCoin(new Coin(Denomination.NICKEL, 1990));
+        purse.addCoin(new Coin(Denomination.QUARTER, 2000));
+
+        System.out.println("Total Value: " + purse.totalValue());
+        System.out.println("Total Weight: " + purse.totalWeight());
+        for (Coin coin : purse.coins) {
+            System.out.println(coin);
+        }
+    }
+}
