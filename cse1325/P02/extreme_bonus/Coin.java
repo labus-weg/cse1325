c44a2a11b14fa66ff7eeceaf90e3b993ba3ff065
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-09-03 08:55:57 +0000

    Add getWeight method to Coin class based on year and denomination

---
+++ cse1325/P02/extreme_bonus/Coin.java
@@ -0,0 +1,32 @@
+public class Coin {
+    private Denomination denomination;
+    private int year;
+
+    public Coin(Denomination denomination, int year) {
+        this.denomination = denomination;
+        this.year = year;
+    }
+
+    public double getValue() {
+        return denomination.getValue();
+    }
+
+    public double getWeight() {
+        switch (denomination) {
+            case PENNY:
+                return (year < 1983) ? 3.110 : 2.500;
+            case NICKEL:
+                return 5.000;
+            case DIME:
+                return (year < 1965) ? 2.500 : 2.268;
+            case QUARTER:
+                return (year < 1965) ? 6.250 : 5.670;
+            default:
+                return 0.0;
+        }
+    }
+
+    public String toString() {
+        return year + " " + denomination;
+    }
+}
