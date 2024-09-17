9057da2f469a9b8ef1ef8d6f2a54fd0a64b7b50b
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-09-17 07:12:10 +0000

    Made Account abstract and defined play as an abstract method

--- P04/customer/Account.java
+++ P04/customer/Account.java
@@ -1,9 +1,16 @@
 // Copyright 2024 by [Nafisa Nawrin Labonno]
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/**
+ * Represents an abstract account for managing media playback.
+ * @author Nafisa Nawrin Labonno
+ * @version 1.0
+ * @since 2024
+ */
+package customer;
 
-package product;
+import product.Media;
 
-public class Account {
+public abstract class Account {
     private static int accountCounter = 0;  // To generate unique account numbers
     private int accountNumber;
 
@@ -15,8 +22,6 @@ public class Account {
         return accountNumber;
     }
 
-    public String play(Media media) {
-        return "Playing " + media;
-   
-    }
+    // Abstract method with no body
+    public abstract String play(Media media);
 }
