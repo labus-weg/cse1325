4e4e18d43ce29d14583ce1b235927dc6d47741dd
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-09-17 07:12:21 +0000

    Implement play method in Unlimited class

---
+++ P04/customer/Unlimited.java
@@ -0,0 +1,24 @@
+// Copyright 2024 by [Nafisa Nawrin Labonno]
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+
+/**
+ * Represents an account with unlimited media access.
+ * @author Nafisa Nawrin Labonno
+ * @version 1.0
+ * @since 2024
+ */
+package customer;
+
+import product.Media;
+
+public class Unlimited extends Account {
+    public Unlimited() {
+        // Constructor can be empty if no additional initialization is needed
+    }
+
+    @Override
+    public String play(Media media) {
+        return "Playing " + media;
+    }
+}
