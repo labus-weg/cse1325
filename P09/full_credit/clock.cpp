fd600a2607728745f94fd9bfd3305562c9c71319
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-11-05 07:25:50 +0000

    Updated clock.cpp

--- P09/full_credit/clock.cpp
+++ P09/full_credit/clock.cpp
@@ -1,31 +1,36 @@
-#include <iostream>
-#include <ostream>
+#include "clock.h"
 
-class Clock {
-	public:
-		Clock(int hours, int minutes, int seconds)
-		: _hours{int}, _minutes{int}, _seconds{int} {
-		virtual Clock()	
-		}
+Clock::Clock(int hours, int minutes, int seconds) : _hours(hours), _minutes(minutes), _seconds(seconds) {
+    if (hours < 0 || hours > 23) {
+        throw std::out_of_range("Invalid hours: " + std::to_string(hours));
+    }
+    if (minutes < 0 || minutes > 59) {
+        throw std::out_of_range("Invalid minutes: " + std::to_string(minutes));
+    }
+    if (seconds < 0 || seconds > 59) {
+        throw std::out_of_range("Invalid seconds: " + std::to_string(seconds));
+    }
+}
 
+Clock::~Clock() {} 
 
-// #include <iostream>
-// #include <ostream>
-// class Person {
-//  public:
-//  Person(std::string name, int id)
-//  : _name{name}, _id{id} {
-//  notify("Person");
-//  }
-//  friend std::ostream& operator<<(std::ostream& ost, Person& person);
-//  protected:
-//  void notify(std::string classname) {
-//  std::cout << classname << ' ' << _name << " constructed" << std::endl;
-//  }
-//  private:
-//  std::string _name;
-//  int _id;
-// };
-// std::ostream& operator<<(std::ostream& ost, Person& person) {
-//  ost << person._name << " (" << person._id << ')';
-//  return ost;
+void Clock::print() const {
+    std::cout << std::setfill('0') << std::setw(2) << _hours << ":"
+              << std::setfill('0') << std::setw(2) << _minutes << ":"
+              << std::setfill('0') << std::setw(2) << _seconds << std::endl;
+}
+
+void Clock::tic() {
+    _seconds++;
+    if (_seconds == 60) {
+        _seconds = 0;
+        _minutes++;
+        if (_minutes == 60) {
+            _minutes = 0;
+            _hours++;
+            if (_hours == 24) {
+                _hours = 0;
+            }
+        }
+    }
+}
