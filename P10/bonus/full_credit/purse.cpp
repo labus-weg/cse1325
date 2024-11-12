8f02f6d14c4194c73e7c0b48868d4e53269a5559
labus-weg <117149420+labus-weg@users.noreply.github.com>
2024-11-12 06:27:36 +0000

    Bonus update 2, fixed bugs, reordered output

---
+++ P10/bonus/full_credit/purse.cpp
@@ -0,0 +1,54 @@
+#include "purse.h"
+#include <iostream>
+
+Purse::Purse(int pounds, int shillings, int pence) : _pounds(pounds), _shillings(shillings), _pence(pence) {
+    rationalize();
+}
+
+std::ostream& operator<<(std::ostream& os, const Purse& purse) {
+    os << "£" << purse._pounds << " " << purse._shillings << "s" << " " << purse._pence << "d";
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Purse& purse) {
+    char poundSymbol, s, d;
+    int pounds, shillings, pence;
+    is >> poundSymbol >> pounds >> shillings >> s >> pence >> d;
+    purse = Purse(pounds, shillings, pence);
+    return is;
+}
+
+Purse Purse::operator+(const Purse& other) const {
+    return Purse(_pounds + other._pounds, _shillings + other._shillings, _pence + other._pence);
+}
+
+Purse& Purse::operator+=(const Purse& other) {
+    _pounds += other._pounds;
+    _shillings += other._shillings;
+    _pence += other._pence;
+    rationalize();
+    return *this;
+}
+
+Purse Purse::operator-(const Purse& other) const {
+    return Purse(_pounds - other._pounds, _shillings - other._shillings, _pence - other._pence);
+}
+
+void Purse::rationalize() {
+    if (_pence >= 12) {
+        _shillings += _pence / 12;
+        _pence %= 12;
+    }
+    if (_shillings >= 20) {
+        _pounds += _shillings / 20;
+        _shillings %= 20;
+    }
+    if (_pence < 0) {
+        _shillings -= (-_pence + 11) / 12;
+        _pence = 12 - (-_pence % 12);
+    }
+    if (_shillings < 0) {
+        _pounds -= (-_shillings + 19) / 20;
+        _shillings = 20 - (-_shillings % 20);
+    }
+}
